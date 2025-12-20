cc1e898bf3fcfe1eb52b7a82c7122ae5a81c13e3
khushi <khushishorey2005@gmail.com>
2025-12-21 00:19:33 +0530

    Basic server setup, backend is running

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 });
