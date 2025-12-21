36a94657f7c5933a17e8923c679e0d27b2696658
khushi <khushishorey2005@gmail.com>
2025-12-21 23:20:42 +0530

    Write the restir and login user api's and updated the authRoute file

---
+++ server/controllers/authController.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const registerUser = async(req,res) => {
+    const {name, email, password} = req.body;
+
+    try {
+        const user = await User.findOne({email});
+        if(user) {
+            return res.status(400).json({message: "User with this email already exists"});
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password,salt);
+
+        user = await User.create({
+            name,
+            email,
+            password: hashPassword,
+        });
+
+        res.status(201).json({message: "User registered successfully"});
+    } catch(error){
+        res.status(500).json({message:"Server error"});
+    }
+}
+
+const loginUser = async (req,res) => {
+    const {email,password} = req.body;
+
+    try {
+        const user = await User.findOne({email});
+        if(!user) {
+            res.status(400).json({message: "Invalid Credentials"});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            res.status(400).json({message: "Invalid Credentials"});
+        }
+
+        const token = jwt.sign(
+            {id: user.__id},
+            process.env.JWT_SECRET,
+            {expiresIn: "1d"}
+        );
+        res.json(token);
+    } catch(error) {
+        res.status(500).json({message:"Server error"});
+    } 
+}
+
+module.exports = {registerUser, loginUser}
--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const router = require("./routes/authRoute")
 
 dotenv.config();
 connectDB();
 
 const app = express();
-// app.use(express.json());
+app.use(express.json());
+
+app.use("/api/auth", router);
+app.use("/api/tasks", router);
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
